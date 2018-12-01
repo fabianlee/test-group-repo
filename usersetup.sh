bdc304c4087b2465802e373c4e5e4e9b32a706c2
flee999 <flee999@hotmail.com>
2018-12-01 17:57:48 -0500

    user setup fixed

--- usersetup.sh
+++ usersetup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # create group named 'mygroup'
-sudo groupadd -u 19001
+sudo groupadd -g 19001 mygroup
 
 # create alice user
 sudo useradd -u 20001 -m alice -s /bin/bash
 # set static password and ensure group definition added to /etc/group
 phash=$(openssl passwd -1 -salt mysalt alicepass)
-sudo usermod -p "$phash" alice
+sudo usermod -p "$phash" -G mygroup alice
 
 # create bob user
 sudo useradd -u 20002 -m bob -s /bin/bash
 # set static password and ensure group definition added to /etc/group
 phash=$(openssl passwd -1 -salt mysalt bobpass)
-sudo usermod -p "$phash" bob
+sudo usermod -p "$phash" -G mygroup bob 
 
